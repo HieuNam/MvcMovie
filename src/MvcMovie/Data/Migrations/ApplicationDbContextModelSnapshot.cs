79b46760c41f181453cc52aebd38e80ef7e26f85
hcvo <jba152@att.net>
2016-10-05 16:23:33 -0500

    Remove unused references

--- src/MvcMovie/Data/Migrations/ApplicationDbContextModelSnapshot.cs
+++ src/MvcMovie/Data/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -2,8 +2,6 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
-using Microsoft.EntityFrameworkCore.Migrations;
-using MvcMovie.Data;
 
 namespace MvcMovie.Data.Migrations
 {
