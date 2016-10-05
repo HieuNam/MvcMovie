3d40275abfb529eaabc23498bfb8cb154f76649b
hcvo <jba152@att.net>
2016-10-05 17:27:05 -0500

    Adding seeddata class

---
+++ src/MvcMovie/Models/SeedData.cs
@@ -0,0 +1,20 @@
+﻿using MvcMovie.Data;
+using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+
+namespace MvcMovie.Models
+{
+    public static class SeedData
+    {
+
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new ApplicationDbContext(serviceProvider.GetRequiredService<DbContextOptions<ApplicationDbContext>>())
+            {
+                //
+            }
+            throw new NotImplementedException();
+        }
+    }
+}
